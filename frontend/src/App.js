4a066cf76bcea8d9b4bf86fab32abf05073557dd
Jolene Chua <jolene.chua@toluna.com>
2023-03-23 16:35:26 +0800

    Fixed formatting, added refresh token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-import Home from './components/home';
-import Login from './components/login';
+import "./App.css";
+import Home from "./components/home";
+import Login from "./components/login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from 'react';
-import { Context } from './utils/context.js';
-// import { get } from './utils/extractdata.js';
-
+import React, { useEffect, useState } from "react";
+import { Context } from "./utils/context.js";
 
 function App() {
   const [ACCESS_TOKEN, setAccessToken] = useState("");
@@ -19,9 +17,10 @@ function App() {
   const getToken = async () => {
     function getHashparams() {
       var hashParams = {};
-      var e, r = /([^&;=]+)=?([^&;]*)/g,
+      var e,
+        r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
-      e = r.exec(q)
+      e = r.exec(q);
       while (e) {
         hashParams[e[1]] = decodeURIComponent(e[2]);
         e = r.exec(q);
@@ -32,20 +31,22 @@ function App() {
     console.log(ACCESS_TOKEN);
     setAccessToken(data.access_token);
     setRefreshToken(data.refresh_token);
-  }
-
+  };
 
   return (
     // Context.provider allows ACCESS_TOKEN to be used in all components inside it
-    <Context.Provider value={ACCESS_TOKEN}> 
-     <BrowserRouter>
+    <Context.Provider value={ACCESS_TOKEN}>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={ACCESS_TOKEN ? <Home /> : <Login />} />
+          <Route
+            path="/"
+            exact
+            element={ACCESS_TOKEN ? <Home /> : <Login />}
+          />
         </Routes>
       </BrowserRouter>
     </Context.Provider>
-
-  )
-};
+  );
+}
 
 export default App;
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,101 +1,122 @@
-import React from 'react';
+import React from "react";
 import { useState, useContext, useEffect } from "react";
-import { Context } from '../utils/context.js';
-import queryString from 'querystring';
-import { get } from '../utils/get.js';  // function to send request to API 
+import { Context } from "../utils/context.js";
+import queryString from "querystring";
+import { get } from "../utils/get.js"; // function to send request to API
 
 function Home() {
+  const ACCESS_TOKEN = useContext(Context);
+  const [profile, setProfile] = useState({}); // profile is an obj hence {}
+  const [TopArtists, setTopArtists] = useState([]); // array
+  const [TopTracks, setTopTracks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const ACCESS_TOKEN = useContext(Context);
-    const [profile, setProfile] = useState({});         // profile is an obj hence {}
-    const [TopArtists, setTopArtists] = useState([]);   // array
-    const [TopTracks, setTopTracks] = useState([]);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Get profile data
+    const getProfile = async () => {
+      const profile = await get(
+        "https://api.spotify.com/v1/me",
+        "GET",
+        ACCESS_TOKEN
+      );
 
-    useEffect(() => {
+      setProfile(profile);
+      // console.log(profile);
+    };
 
-        // Get profile data
-        const getProfile = async () => {
-            const profile = await get('https://api.spotify.com/v1/me', 'GET', ACCESS_TOKEN);
+    // Get top artists
+    const getTopArtists = async () => {
+      const response = await get(
+        "https://api.spotify.com/v1/me/top/artists",
+        "GET",
+        ACCESS_TOKEN
+      );
+      const TopArtists = await response.items.map(function (d) {
+        return { name: d.name, id: d.id, genres: d.genres };
+      });
 
-            setProfile(profile);
-            // console.log(profile);
-        };
+      setTopArtists(TopArtists);
+      // console.log(TopArtists)
+    };
 
-        // Get top artists
-        const getTopArtists = async () => {
-            const response = await get('https://api.spotify.com/v1/me/top/artists', 'GET', ACCESS_TOKEN);
-            const TopArtists = await response.items.map(function (d) { return { name: d.name, id: d.id, genres: d.genres } });
+    // Get top tracks
+    const getTopTracks = async () => {
+      const response = await get(
+        "https://api.spotify.com/v1/me/top/tracks?" +
+          queryString.stringify({
+            limit: "50",
+            time_range: "long_term",
+          }),
+        "GET",
+        ACCESS_TOKEN
+      );
+      const TopTracks = await response.items.map(function (d) {
+        return { name: d.name, id: d.id, album: d.album.name };
+      });
 
-            setTopArtists(TopArtists);
-            // console.log(TopArtists)
-        };
+      // Note: Do not run setState twice (i.e. once here and once in getAudioFeatures)
+      return TopTracks;
+      // getAudioFeatures(TopTracks);
+    };
 
-        // Get top tracks
-        const getTopTracks = async () => {
-            const response = await get('https://api.spotify.com/v1/me/top/tracks?' +
-                queryString.stringify({
-                    limit: '50',
-                    time_range: 'long_term'
-                }), 'GET', ACCESS_TOKEN);
-            const TopTracks = await response.items.map(function (d) { return { name: d.name, id: d.id, album: d.album.name } });
+    // Get audio features of tracks
+    const getAudioFeatures = async (tracks) => {
+      const feat = await get(
+        "https://api.spotify.com/v1/audio-features?" +
+          queryString.stringify({
+            ids: tracks.map((d) => d.id).join(","),
+          }),
+        "GET",
+        ACCESS_TOKEN
+      );
+      const TopTracksFeat = await tracks.map((d, index) => {
+        return { ...d, features: feat.audio_features[index] };
+      });
 
-            // Note: Do not run setState twice (i.e. once here and once in getAudioFeatures)
-            return TopTracks;
-            // getAudioFeatures(TopTracks);
-        };
+      setTopTracks(TopTracksFeat);
+      // console.log(TopTracksFeat[0].features.valence);
+      console.log(TopTracksFeat);
+    };
 
-        // Get audio features of tracks
-        const getAudioFeatures = async (tracks) => {
-            const feat = await get('https://api.spotify.com/v1/audio-features?' +
-                queryString.stringify({
-                    ids: tracks.map(d => d.id).join(',')
-                }), 'GET', ACCESS_TOKEN);
-            const TopTracksFeat = await tracks.map((d, index) => {
-                return { ...d, features: feat.audio_features[index] }
-            })
+    // Run all async functions in parallel first. When they are done, set loading to false
+    Promise.all([
+      getProfile(),
+      getTopArtists(),
+      getTopTracks().then((d) => getAudioFeatures(d)),
+    ]).then(() => {
+      setLoading(false);
+      console.log("Done!");
+    });
+  }, []);
 
-            setTopTracks(TopTracksFeat);
-            // console.log(TopTracksFeat[0].features.valence);
-            console.log(TopTracksFeat);
-        };
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-        // Run all async functions in parallel first. When they are done, set loading to false
-        Promise.all(
-            [getProfile(),
-            getTopArtists(),
-            getTopTracks().then(d => getAudioFeatures(d))])
-            .then(() => {
-                setLoading(false);
-                console.log('Done!');
-            });
-
-    }, []);
-
-    if (loading) {
-        return (
-            <div>Loading...</div>
-        )
-    }
-
-    return (
-        <>
-            <div>Home</div>
-            <div>{ACCESS_TOKEN}</div>
-            <div>{ACCESS_TOKEN ? <div>Profile: {profile.display_name}</div> : 'null'}</div>
-            <div>Top Artists {JSON.stringify(TopArtists.name)}</div>
-            <ol>
-                {TopArtists.map(d => (
-                    <li key={d.name}>{d.name} Genre: {d.genres[0]} </li>
-                ))}
-            </ol>
-            <ol>
-                {TopTracks.map(d => (
-                    <li key={d.name}>{d.name} {d.features.valence}</li>
-                ))}
-            </ol>
-        </>
-    )
-};
+  return (
+    <>
+      <div>Home</div>
+      <div>{ACCESS_TOKEN}</div>
+      <div>
+        {ACCESS_TOKEN ? <div>Profile: {profile.display_name}</div> : "null"}
+      </div>
+      <div>Top Artists {JSON.stringify(TopArtists.name)}</div>
+      <ol>
+        {TopArtists.map((d) => (
+          <li key={d.name}>
+            {d.name} Genre: {d.genres[0]}{" "}
+          </li>
+        ))}
+      </ol>
+      <ol>
+        {TopTracks.map((d) => (
+          <li key={d.name}>
+            {d.name} {d.features.valence}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
 
 export default Home;
